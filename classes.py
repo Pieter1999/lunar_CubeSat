8d5b5bf6879d0e4d32d147f658db5ad2b0ad02b7
pieterdelange <P.deLange@student.tudelft.net>
2024-04-25 15:07:41 +0200

    First .py files

---
+++ classes.py
@@ -0,0 +1,17 @@
+class Constants:
+    """
+    This class contains all the constants that are used throughout the research. Constants can in this way
+    easily be called and their values are stored in one central location. Further in-line comments will
+    clarify the meaning and values of the constants present.
+    """
+
+    # Physical constants
+    G = 6.67428e-11  # Universal Gravitational Constant [m^3s^-2kg^-1]
+
+
+class DisturbanceTorques:
+    """ """
+
+class EphemerisData:
+    """ """
+
